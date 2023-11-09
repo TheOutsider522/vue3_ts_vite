68c7fb98c2ffb7df6f2df2aa2d1e9f79d7798756
songxueliang <18300206590@163.com>
2023-11-09 20:35:59 +0800

    测试代码格式化提交

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import App from './App.vue'
 
 createApp(App).mount('#app')
 
-const fun = () => {
-  console.log('vue3 + JavaScript + Vite')
+const fun = () =>
+{
+console.log('vue3 + JavaScript + Vite');
 }
 
-fun()
+fun();
